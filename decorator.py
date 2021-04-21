aa942e08870f2f310a065a0faffa19b8fa821861
Edgar A <eoalbert@gmail.com>
2021-04-21 18:36:16 +0200

    default template for decorators

--- decorator.py
+++ decorator.py
@@ -4,6 +4,7 @@ import functools
 
 
 def decorator(func):
+    """Simple decorator without arguments"""
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -17,3 +18,22 @@ def decorator(func):
 @decorator
 def func(a,b):
     return a+b
+
+def repeat(num_times):
+
+    def decorator_with_arguments(func):
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            #Do...
+            for _ in range(num_times):
+                result = func(*args, **kwargs)
+            #Do after...
+            return result
+        return wrapper
+    return decorator_with_arguments
+
+
+@repeat(num_times=4)
+def greet(name):
+    print(f'Hello {name}')
